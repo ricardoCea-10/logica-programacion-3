a181e22a5eeae4a047d0e2d531992dbe90eb3e3c
ricardoCea <ricardo.cea@genstudents.org>
2024-09-26 11:34:17 -0300

    Laboratorio - Logica de programacion 3 - Ricardo Cea

---
+++ index.js
@@ -0,0 +1,32 @@
+// const prompt = require('prompt-sync')();
+
+// El siguiente programa debe solicitar al usuario un número por prompt o por input y guardarlo.
+// Y debe ser capaz de identificar si el dato de entrada es de tipo number, en caso contrario debe mandar un mensaje de error y volver a solicitar el dato.
+let numero;
+do {
+    numero = prompt('Ingrese un número: ');
+    numero = Number(numero); // convertir a número.
+    if (isNaN(numero)) {
+        console.log('Error: Ingrese un número válido.');
+        alert('Error: Ingrese un número válido.');
+    }
+} while (isNaN(numero)); // valor no es número
+console.log("Haz ingresado el número " + numero);
+console.log("")
+
+// Se debe calcular el factorial del número recibido.
+let resultado = factorial(numero);
+console.log("El factorial de " + numero + " es " + resultado);
+
+// Debe imprimir el resultado por el DOM de un archivo index.html que debes crear en el mismo directorio de tu proyecto.
+document.getElementById("resultado").innerHTML = 
+    `El factorial de ${numero} calculado es: <br>
+    ${resultado}`;
+
+function factorial(numero) {
+    let resultado = 1;
+    for (let i = 1; i <= numero; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
